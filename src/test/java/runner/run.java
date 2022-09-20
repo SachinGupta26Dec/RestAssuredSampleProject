8a303f1a4c47311840561a136fd222207ac6672a
Sachin Gupta <sachin_26dec@yahoo.co.in>
2022-09-20 22:43:20 +0530

    Update run.java

--- src/test/java/runner/run.java
+++ src/test/java/runner/run.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features = { "src/test/resources/features" }, glue = {
 		"stepdef" }, monochrome = true, plugin = { "pretty", "json:target/jsonFolder/cucumber-report.json",
 				"html:target/htmlFolder/cucumber-report/index.html", "rerun:target/rerunFolder/rerun.txt",
-				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","timeline:target/timelineFolder/" })
 
 public class run extends AbstractTestNGCucumberTests {
 
